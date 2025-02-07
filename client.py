8472b9bd5132db6b6ad37a98f87a9d72d529be3f
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2025-02-07 07:24:55 -0600

    Working on streaming.

--- client.py
+++ client.py
@@ -1,17 +1,30 @@
 import requests
+import io
+import sounddevice as sd
+import soundfile as sf
 
-HOST = 'http://localhost:8000'
-
-# Get the wave file from the server
+HOST = "http://0.0.0.0:8000"
 url = f"{HOST}/node/speech"
-response = requests.get(url, params={
-    "text": "What's up baby?",
-    "voice": "af_bella",
-    "speed": 0.7,
-    "split_pattern": r"\n+",
-})
 
-print(response.status_code)
+response = requests.get(
+    url,
+    params={
+        "text": "How are you doing there you, sexy ass, mother fucker?",
+        "voice": "af_bella",
+        "speed": 1.1,
+        "split_pattern": r"\n+",
+    },
+    stream=True,
+)
 
-with open('retrieved_audi.wav', 'wb') as f:
-    f.write(response.content)
+# Read the streamed response into memory
+audio_buffer = io.BytesIO()
+for chunk in response.iter_content(chunk_size=4096):
+    if chunk:
+        audio_buffer.write(chunk)
+
+# Play the audio in real-time
+audio_buffer.seek(0)  # Reset buffer for reading
+data, samplerate = sf.read(audio_buffer)
+sd.play(data, samplerate)
+sd.wait()  # Wait for audio to finish playing
--- speech_node/kokoro_server.py
+++ speech_node/kokoro_server.py
@@ -1,14 +1,15 @@
+import io
 from kokoro import KPipeline
 import soundfile as sf
 from typing import Union
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+
+# from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from uuid import uuid4
 
-# class KokoroServer:
-
-#     def __init__(self):
 app = FastAPI()
+
 
 @app.get(
     path="/node/speech",
@@ -19,7 +20,7 @@ async def generate_speech(
     speed: Union[float, int],
     split_pattern: str,
 ):
-    
+
     unique_id = str(uuid4())
 
     pipeline = KPipeline(
@@ -33,9 +34,18 @@ async def generate_speech(
         speed=speed,
         split_pattern=split_pattern,
     )
-    for i, (gs, ps, audio) in enumerate(generator):
-        filename = f"{unique_id}_{i}.wav"
-        sf.write(filename, audio, 24000)
 
-    return FileResponse(filename, media_type="audio/mpeg")
+    def iterfile():
+        buffer = io.BytesIO()
+        for _, _, audio in generator:
+            sf.write(
+                buffer,
+                audio,
+                samplerate=24000,
+                format="WAV",
+            )
+            buffer.seek(0)
+            yield buffer.read()
+            buffer.truncate(0)
 
+    return StreamingResponse(iterfile(), media_type="audio/wav")
