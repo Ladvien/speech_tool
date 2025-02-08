646e0998497054dfb7f05e92b67021ce886debf9
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2025-02-08 07:19:39 -0600

    Getting it done

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ CONFIG_PATH = os.environ.get("NODE_CONFIG_PATH", "config.yaml")
 config = NodeConfig(**yaml.safe_load(open(CONFIG_PATH, "r")))
 
 app = FastAPI()
+
 speech_node = SpeechNodeServer(config)
 app.include_router(speech_node.router)
